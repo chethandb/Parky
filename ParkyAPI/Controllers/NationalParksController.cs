fac4677f78dc4be4196a448999c3892572f4df07
Chethan D. Balakrishna <iamchethandb@gmail.com>
2021-11-26 14:59:18 -0600

    added controller with first Get method to return all national parks

---
+++ ParkyAPI/Controllers/NationalParksController.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ParkyAPI.Models;
+using ParkyAPI.Models.DTO;
+using ParkyAPI.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NationalParksController : Controller
+    {
+
+        private readonly INationalParkRepository _nationalParkRepository;
+        private readonly IMapper _mapper;
+
+        public NationalParksController(INationalParkRepository nationalParkRepository,
+                                       IMapper mapper)
+        {
+            _nationalParkRepository = nationalParkRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetNationalParks()
+        {
+            var objList = _nationalParkRepository.GetNationalParks();
+
+            var objDTO = new List<NationalParkDTO>();
+
+            foreach (var item in objList)
+            {
+                objDTO.Add(_mapper.Map<NationalParkDTO>(item));
+            }
+
+            return Ok(objDTO);
+        }
+    }
+}
