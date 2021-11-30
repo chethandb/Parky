24c467d16aef2afaec7601a0a8737f670d66135c
Chethan D. Balakrishna <iamchethandb@gmail.com>
2021-11-30 00:21:35 -0600

    added trail model/dto

---
+++ ParkyAPI/Models/DTO/TrailDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static ParkyAPI.Models.Trail;
+
+namespace ParkyAPI.Models.DTO
+{
+    public class TrailDTO
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public double Distance { get; set; }
+
+        public DifficultyType Difficulty { get; set; }
+
+        [Required]
+        public int NationalParkId { get; set; }
+
+        public NationalPark NationalPark { get; set; }
+    }
+}
