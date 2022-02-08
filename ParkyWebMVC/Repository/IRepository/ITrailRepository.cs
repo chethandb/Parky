b51a3d9bc9d364d34265085061cabe15fa97c00b
Chethan D. Balakrishna <iamchethandb@gmail.com>
2022-02-08 00:06:54 -0600

    NP and Trail repo initial creation

---
+++ ParkyWebMVC/Repository/IRepository/ITrailRepository.cs
@@ -0,0 +1,12 @@
+﻿using ParkyWebMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyWebMVC.Repository.IRepository
+{
+    interface ITrailRepository : IRepository<Trail>
+    {
+    }
+}
---
+++ ParkyWebMVC/Repository/TrailRepository.cs
@@ -0,0 +1,20 @@
+﻿using ParkyWebMVC.Models;
+using ParkyWebMVC.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ParkyWebMVC.Repository
+{
+    public class TrailRepository : Repository<Trail>, ITrailRepository
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public TrailRepository(IHttpClientFactory httpClientFactory) : base(httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+    }
+}
