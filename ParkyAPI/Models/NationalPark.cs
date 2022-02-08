774b04b78406746d43939a52525fe00d081e7bca
Chethan D. Balakrishna <iamchethandb@gmail.com>
2022-02-07 19:37:30 -0600

    added an image property for NP

--- ParkyAPI/Models/NationalPark.cs
+++ ParkyAPI/Models/NationalPark.cs
@@ -11,6 +11,8 @@ namespace ParkyAPI.Models
         public string Name { get; set; }
         [Required]
         public string State { get; set; }
+
+        public byte[] Picture { get; set; }
         public DateTime Created { get; set; }
         public DateTime Established { get; set; }
 
