43f70da3f216a782fb93cd549ed3ef322bfc274c
Chethan D. Balakrishna <iamchethandb@gmail.com>
2022-02-07 23:05:44 -0600

    added static details for api access

---
+++ ParkyWebMVC/StaticDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyWebMVC
+{
+    public static class StaticDetails
+    {
+        public static string APIBaseURL = "https://localhost:44374/";
+        public static string NationalParkAPIPath = APIBaseURL + "api/v1/nationalparks";
+        public static string TrailAPIPath = APIBaseURL + "api/v1/trails";
+    }
+}
