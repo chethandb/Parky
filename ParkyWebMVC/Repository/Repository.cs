51331a90208bd08adb2529fcb74cffe38c85f2e2
Chethan D. Balakrishna <iamchethandb@gmail.com>
2022-02-07 23:43:17 -0600

    completed other methods in repository

--- ParkyWebMVC/Repository/Repository.cs
+++ ParkyWebMVC/Repository/Repository.cs
@@ -3,6 +3,7 @@ using ParkyWebMVC.Repository.IRepository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,7 +22,7 @@ namespace ParkyWebMVC.Repository
         public async Task<bool> CreateAsync(string url, T objToCreate)
         {
             var request = new HttpRequestMessage(HttpMethod.Post, url);
-            if(objToCreate != null)
+            if (objToCreate != null)
             {
                 request.Content = new StringContent(JsonConvert.SerializeObject(objToCreate),
                                                     Encoding.UTF8, "application/json");
@@ -33,7 +34,7 @@ namespace ParkyWebMVC.Repository
 
             var client = _httpClientFactory.CreateClient();
             HttpResponseMessage response = await client.SendAsync(request);
-            if(response.StatusCode == System.Net.HttpStatusCode.Created)
+            if (response.StatusCode == HttpStatusCode.Created)
             {
                 return true;
             }
@@ -43,24 +44,77 @@ namespace ParkyWebMVC.Repository
             }
         }
 
-        public Task<bool> DeleteAsync(string url, int Id)
+        public async Task<bool> DeleteAsync(string url, int Id)
         {
-            throw new NotImplementedException();
+            var request = new HttpRequestMessage(HttpMethod.Delete, url + Id);
+
+            var client = _httpClientFactory.CreateClient();
+            HttpResponseMessage response = await client.SendAsync(request);
+
+            if (response.StatusCode == HttpStatusCode.NoContent)
+            {
+                return true;
+            }
+
+            return false;
         }
 
-        public Task<IEnumerable<T>> GetAllAsync(string url)
+        public async Task<IEnumerable<T>> GetAllAsync(string url)
         {
-            throw new NotImplementedException();
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+
+            var client = _httpClientFactory.CreateClient();
+            HttpResponseMessage response = await client.SendAsync(request);
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var jsonString = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<IEnumerable<T>>(jsonString);
+            }
+
+            return null;
         }
 
-        public Task<T> GetAsync(string url, int Id)
+        public async Task<T> GetAsync(string url, int Id)
         {
-            throw new NotImplementedException();
+            var request = new HttpRequestMessage(HttpMethod.Get, url + Id);
+
+            var client = _httpClientFactory.CreateClient();
+            HttpResponseMessage response = await client.SendAsync(request);
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var jsonString = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<T>(jsonString);
+            }
+
+            return null;
         }
 
-        public Task<bool> UpdateAsync(string url, T objToUpdate)
+        public async Task<bool> UpdateAsync(string url, T objToUpdate)
         {
-            throw new NotImplementedException();
+            var request = new HttpRequestMessage(HttpMethod.Patch, url);
+            if (objToUpdate != null)
+            {
+                request.Content = new StringContent(JsonConvert.SerializeObject(objToUpdate),
+                                                    Encoding.UTF8, "application/json");
+            }
+            else
+            {
+                return false;
+            }
+
+            var client = _httpClientFactory.CreateClient();
+            HttpResponseMessage response = await client.SendAsync(request);
+
+            if (response.StatusCode == HttpStatusCode.NoContent)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
