5ad5553161dd6416ab361f9b99ad5cafe6f15e65
Chethan D. Balakrishna <iamchethandb@gmail.com>
2022-02-08 23:01:06 -0600

    wrapping up upsert view

--- ParkyWebMVC/StaticDetails.cs
+++ ParkyWebMVC/StaticDetails.cs
@@ -3,7 +3,7 @@
     public static class StaticDetails
     {
         public static string APIBaseURL = "https://localhost:44374/";
-        public static string NationalParkAPIPath = APIBaseURL + "api/v1/nationalparks";
-        public static string TrailAPIPath = APIBaseURL + "api/v1/trails";
+        public static string NationalParkAPIPath = APIBaseURL + "api/v1/nationalparks/";
+        public static string TrailAPIPath = APIBaseURL + "api/v1/trails/";
     }
 }
