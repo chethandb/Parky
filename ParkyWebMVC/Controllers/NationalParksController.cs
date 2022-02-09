cdb58c149ad6b150886b47f745bce4dc6a7e81c2
Chethan D. Balakrishna <iamchethandb@gmail.com>
2022-02-08 23:44:53 -0600

    wrapped up delete national park

--- ParkyWebMVC/Controllers/NationalParksController.cs
+++ ParkyWebMVC/Controllers/NationalParksController.cs
@@ -3,6 +3,7 @@ using ParkyWebMVC.Models;
 using ParkyWebMVC.Repository.IRepository;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -43,9 +44,63 @@ namespace ParkyWebMVC.Controllers
             return View(obj);
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(NationalPark obj)
+        {
+            if (ModelState.IsValid)
+            {
+                var file = HttpContext.Request.Form.Files;
+                if (file.Count > 0)
+                {
+                    byte[] p1 = null;
+                    using (var fs1 = file[0].OpenReadStream())
+                    {
+                        using (var ms1 = new MemoryStream())
+                        {
+                            fs1.CopyTo(ms1);
+                            p1 = ms1.ToArray();
+                        }
+                    }
+
+                    obj.Picture = p1;
+                }
+                else
+                {
+                    var objFromDb = await _nationalParkRepository.GetAsync(StaticDetails.NationalParkAPIPath, obj.Id);
+                    obj.Picture = objFromDb.Picture;
+                }
+
+                if (obj.Id == 0)
+                {
+                    await _nationalParkRepository.CreateAsync(StaticDetails.NationalParkAPIPath, obj);
+                }
+                else
+                {
+                    await _nationalParkRepository.UpdateAsync(StaticDetails.NationalParkAPIPath + obj.Id, obj);
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View(obj);
+            }
+        }
+
         public async Task<IActionResult> GetAllNationalPark()
         {
             return Json(new { data = await _nationalParkRepository.GetAllAsync(StaticDetails.NationalParkAPIPath) });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var status = await _nationalParkRepository.DeleteAsync(StaticDetails.NationalParkAPIPath, id);
+            if (status)
+            {
+                return Json(new { success = true, message = "Delete Successful" });
+            }
+            return Json(new { success = true, message = "Delete Not Successful" });
         }
     }
 }
